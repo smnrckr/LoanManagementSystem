b6c1f2842a95374ffde52c082392c84bc03ff336
Semanur Çakır <semanurcakir11@gmail.com>
2024-08-07 10:27:17 +0300

    New client applications process completed

--- src/LoanTable.js
+++ src/LoanTable.js
@@ -25,7 +25,7 @@ const LoanTable = () => {
       });
   }, [userCode, navigate]);
 
-  const handleNewApplicationRedirect=()=>{
+  const handleNewApplicationRedirect = () => {
     navigate(`/newApplication/${userCode}`);
   };
 
@@ -52,7 +52,6 @@ const LoanTable = () => {
     marginBottom: "10px",
     width: "25%",
     borderCollapse: "collapse",
-    
   };
 
   return (
@@ -72,42 +71,41 @@ const LoanTable = () => {
           ) : (
             <>
               <table border={1}>
-  <thead>
-    <tr>
-      <th style={thStyle}>TCKN</th>
-      <th>Telefon</th>
-      <th>İsim</th>
-      <th>Soyisim</th>
-      <th>Email</th>
-      <th>Adres</th>
-      <th>Aylık Gelir</th>
-      <th>Kampanya Adı</th>
-      <th>Kredi Tarihi</th>
-      <th>Kredi Tutarı</th>
-      <th>Kredi Oranı</th>
-      <th>Vade Süresi</th>
-    </tr>
-  </thead>
-  <tbody>
-    {borrowerData.map((borrower) => (
-      <tr key={borrower.id}>
-        <td style={tdStyle}>{borrower.tckn}</td>
-        <td>{borrower.phoneNumber}</td>
-        <td>{borrower.name}</td>
-        <td>{borrower.surname}</td>
-        <td>{borrower.emailClient}</td>
-        <td>{borrower.address}</td>
-        <td>{borrower.monthlySalary}</td>
-        <td>{borrower.campaignName}</td>
-        <td>{borrower.loanDate}</td>
-        <td>{borrower.loanAmount}</td>
-        <td>{borrower.interestRate}</td>
-        <td>{borrower.termLoan}</td>
-      </tr>
-    ))}
-  </tbody>
-</table>
-
+                <thead>
+                  <tr>
+                    <th style={thStyle}>TCKN</th>
+                    <th>Telefon</th>
+                    <th>İsim</th>
+                    <th>Soyisim</th>
+                    <th>Email</th>
+                    <th>Adres</th>
+                    <th>Aylık Gelir</th>
+                    <th>Kampanya Adı</th>
+                    <th>Kredi Tarihi</th>
+                    <th>Kredi Tutarı</th>
+                    <th>Kredi Oranı</th>
+                    <th>Vade Süresi</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {borrowerData.map((borrower) => (
+                    <tr key={borrower.id}>
+                      <td style={tdStyle}>{borrower.tckn}</td>
+                      <td>{borrower.phoneNumber}</td>
+                      <td>{borrower.name}</td>
+                      <td>{borrower.surname}</td>
+                      <td>{borrower.emailClient}</td>
+                      <td>{borrower.address}</td>
+                      <td>{borrower.monthlySalary}</td>
+                      <td>{borrower.campaignName}</td>
+                      <td>{borrower.loanDate}</td>
+                      <td>{borrower.loanAmount}</td>
+                      <td>{borrower.interestRate}</td>
+                      <td>{borrower.termLoan}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
             </>
           )}
         </div>
--- src/NewApplication.js
+++ src/NewApplication.js
@@ -5,25 +5,27 @@ const NewApplication = () => {
   const { userCode } = useParams();
   const navigate = useNavigate();
   const [loading, setLoading] = useState(false);
-  const [campaigns,setCampaigns]=useState([]);
-  const [selectedCampaign, setSelectedCampaign] = useState('');
+  const [campaigns, setCampaigns] = useState([]);
+  const [selectedCampaign, setSelectedCampaign] = useState("");
+  const [selectedTerm, setSelectedTerm] = useState("");
+  const [rate, setRate] = useState("");
+  const [loanOptions, setLoanOptions] = useState([]);
+
   const [formData, setFormData] = useState({
     tckn: "",
     phoneNumber: "",
-    firstName: "",
-    lastName: "",
-    email: "",
+    name: "",
+    surname: "",
+    emailClient: "",
     address: "",
-    monthlyIncome: "",
+    monthlySalary: "",
     loanAmount: "",
-    campaignName: "",
-    termLoan: "",
-    interestRate: "",
+    loanDate: "",
   });
 
   useEffect(() => {
     setLoading(true);
-    fetch('http://localhost:8080/api/campaign')
+    fetch("http://localhost:8080/api/campaign")
       .then((response) => {
         return response.json();
       })
@@ -33,99 +35,197 @@ const NewApplication = () => {
       });
   }, [userCode, navigate]);
 
+  useEffect(() => {
+    if (selectedCampaign) {
+      const filteredTerms = campaigns
+        .filter((campaign) => campaign.campaignName === selectedCampaign)
+        .map((campaign) => ({
+          termLoan: campaign.termLoan,
+          interestRate: campaign.interestRate,
+        }));
+      setLoanOptions(filteredTerms);
+    }
+  }, [selectedCampaign, campaigns]);
+
+  useEffect(() => {
+    if (selectedTerm) {
+      const termData = loanOptions.find(
+        (loanOptions) => loanOptions.termLoan === parseFloat(selectedTerm)
+      );
+      if (termData) {
+        setRate(termData.interestRate);
+      }
+    }
+  }, [selectedTerm, loanOptions]);
+
+
+
+
+const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const response = await fetch(`http://localhost:8080/api/newApplication/${userCode}`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ ...formData, campaignName: selectedCampaign, termLoan: selectedTerm, interestRate: rate }),
+    });
+
+    const data = await response.json();
+    console.log(data);
+    if (response.ok) {
+      navigate(`/loans/${userCode}`);
+    }
+    
+};
+
+  const comboStyle = {
+    display: "flex",
+    justifyContent: "flex-end",
+    marginBottom: "10px",
+    width: "13%",
+    borderCollapse: "collapse",
+    padding: "1%",
+  };
+  const buttonStyle = {
+    marginBottom: "10px",
+    width: "15%",
+    borderCollapse: "collapse",
+  };
+
   if (loading) return <p>Loading...</p>;
 
   return (
     <div>
+      <h1>Kredi Başvurusu</h1>
       <div>
-      <form>
-        <input
-          type="text"
-          placeholder="TCKN"
-          value={formData.tckn}
-          onChange={(e) => setFormData({ ...formData, tckn: e.target.value })}
-          required
-        />
-        <input
-          type="text"
-          placeholder="Telefon"
-          value={formData.phoneNumber}
-          onChange={(e) => setFormData({ ...formData, phoneNumber: e.target.value })}
-          required
-        />
-        <input
-          type="text"
-          placeholder="Ad"
-          value={formData.firstName}
-          onChange={(e) =>
-            setFormData({ ...formData, firstName: e.target.value })
-          }
-          required
-        />
-        <input
-          type="text"
-          placeholder="Soyad"
-          value={formData.lastName}
-          onChange={(e) =>
-            setFormData({ ...formData, lastName: e.target.value })
-          }
-          required
-        />
-        <input
-          type="email"
-          placeholder="Email"
-          value={formData.email}
-          onChange={(e) => setFormData({ ...formData, email: e.target.value })}
-          required
-        />
-        <input
-          type="text"
-          placeholder="Adres"
-          value={formData.address}
-          onChange={(e) =>
-            setFormData({ ...formData, address: e.target.value })
-          }
-          required
-        />
-        <input
-          type="number"
-          placeholder="Aylık Gelir"
-          value={formData.monthlyIncome}
-          onChange={(e) =>
-            setFormData({ ...formData, monthlyIncome: e.target.value })
-          }
-          required
-        />
-        <input
-          type="number"
-          placeholder="Kredi Tutarı"
-          value={formData.loanAmount}
-          onChange={(e) =>
-            setFormData({ ...formData, loanAmount: e.target.value })
-          }
-          required
-        />
-        
-      </form>
-    </div>
-    <div>
-    <select
-        value={selectedCampaign}
-        onChange={(e) => setSelectedCampaign(e.target.value)}>
-        <option value="">Bir Kampanya Seçin</option>
-        {campaigns.map((campaign) => (
-          <option key={campaign.id} value={campaign.id}>
-            {campaign.campaignCode} { }
-          </option>
-        ))}
-      </select>
-      <form>
-        
-      </form>
+        <form>
+          <input
+            type="text"
+            placeholder="TCKN"
+            value={formData.tckn}
+            onChange={(e) => setFormData({ ...formData, tckn: e.target.value })}
+            required
+          />
+          <input
+            type="text"
+            placeholder="Telefon"
+            value={formData.phoneNumber}
+            onChange={(e) =>
+              setFormData({ ...formData, phoneNumber: e.target.value })
+            }
+            required
+          />
+          <input
+            type="text"
+            placeholder="Ad"
+            value={formData.name}
+            onChange={(e) =>
+              setFormData({ ...formData, name: e.target.value })
+            }
+            required
+          />
+          <input
+            type="text"
+            placeholder="Soyad"
+            value={formData.surname}
+            onChange={(e) =>
+              setFormData({ ...formData, surname: e.target.value })
+            }
+            required
+          />
+          <input
+            type="email"
+            placeholder="Email"
+            value={formData.emailClient}
+            onChange={(e) =>
+              setFormData({ ...formData, emailClient: e.target.value })
+            }
+            required
+          />
+          <input
+            type="text"
+            placeholder="Adres"
+            value={formData.address}
+            onChange={(e) =>
+              setFormData({ ...formData, address: e.target.value })
+            }
+            required
+          />
+          <input
+            type="number"
+            placeholder="Aylık Gelir"
+            value={formData.monthlySalary}
+            onChange={(e) =>
+              setFormData({ ...formData, monthlySalary: e.target.value })
+            }
+            required
+          />
+          <input
+            type="number"
+            placeholder="Kredi Tutarı"
+            value={formData.loanAmount}
+            onChange={(e) =>
+              setFormData({ ...formData, loanAmount: e.target.value })
+            }
+            required
+          />
+          <input
+            type="text"
+            placeholder="Kredi Tarihi"
+            value={formData.loanDate}
+            onChange={(e) =>
+              setFormData({ ...formData, loanDate: e.target.value })
+            }
+            required
+          />
+        </form>
+      </div>
+      <div>
+        <select
+          style={comboStyle}
+          value={selectedCampaign}
+          onChange={(e) => setSelectedCampaign(e.target.value)}
+        >
+          <option value="">Bir Kampanya Seçin</option>
+          {campaigns.map((campaign) => (
+            <option key={campaign.id} value={campaign.campaignName}>
+              {campaign.campaignName} {}
+            </option>
+          ))}
+        </select>
+        {selectedCampaign && (
+          <>
+            <select
+              style={comboStyle}
+              value={selectedTerm}
+              onChange={(e) => setSelectedTerm(e.target.value)}
+              disabled={!selectedCampaign}
+            >
+              <option value="">Vade Seçimi Yapın</option>
+              {loanOptions.map((loanOptions, index) => (
+                <option key={index} value={loanOptions.termLoan}>
+                  {loanOptions.termLoan}
+                </option>
+              ))}
+            </select>
+            <input
+              style={comboStyle}
+              type="text"
+              value={rate}
+              readOnly
+              placeholder="Kredi Oranı"
+            />
+          </>
+        )}
+      </div>
+      <div>
+        <button style={buttonStyle} onClick={handleSubmit}>
+          Başvuru Yap
+        </button>
       </div>
     </div>
-    
-
   );
 };
 export default NewApplication;
