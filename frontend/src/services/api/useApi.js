edfee50114f3ac1870bcae159378bcafca06bb32
Semanur Çakır <semanurcakir11@gmail.com>
2024-09-03 15:26:09 +0300

    Reapply "Moved project files into backend directory"
    This reverts commit 87445b3535135f773c3defd78ec6330874ab3dd2.

--- frontend/src/services/api/useApi.js
+++
@@ -1,8 +0,0 @@
-import { BASE_API_URL } from  './apiUrl';
-
-import { useFetch } from 'use-http';
-
-const useApi = (options, triggerDependency) =>
-  useFetch(BASE_API_URL, options, triggerDependency);
-
-export default useApi;
